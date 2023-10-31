870317440c326c4e89c251973eddd049f64b1f99
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-10-31 12:18:03 -0600

    Agregar estructura del codigo

---
+++ Listasenlazadas.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Declaracion de la estructura del nodo.
+typedef struct Node {
+    int data;
+    struct Node *next;
+} Node;
+
+// Función encargada de agregar un nodo al final de la lista.
+void addNode() {
+
+
+}
+
+
+// Función encargarda de eliminar nodos.
+void deleteNode() {
+
+
+
+
+}
+
+// Función encargada de  agregar un nodo al inicio de la lista
+void addToFront(){
+
+
+}
+
+
+// Función encargada de imprimir la lista
+void printList() {
+
+
+}
+
+
+// Función para obtener el elemento enésimo
+void getElementAt() {
+
+
+}
+
+
+// Función para liberar la memoria de la lista.
+void freeList() {
+
+
+
+}
+
+
+// Función main. Encargada de llamar la demas funciones.
+int main() {
+
+
+}
