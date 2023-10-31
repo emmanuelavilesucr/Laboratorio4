42224217996463a287d43bf44dce3610fff3c1b2
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-10-31 17:17:16 -0600

    Agregar la funcion main

--- Listasenlazadas.c
+++ Listasenlazadas.c
@@ -55,7 +55,7 @@ void deleteNode(Node **head, int value) {
 
 
 // Función encargada de  agregar un nodo al inicio de la lista
-void addToFrontNode(Node **head, int value) {
+void addToFront(Node **head, int value) {
 
     Node *newNode = (Node *)malloc(sizeof(Node));  // Se reserva la memoria para el nuevo nodo
     newNode->data = value; // Se establece un valor para el nuevo nodo
@@ -123,5 +123,46 @@ void freeList(Node *head) {
 // Función main. Encargada de llamar la demas funciones.
 int main() {
 
+    Node *head = NULL;
 
+// En esta parte se llama la funcion addNode y se establecen los numeros de la lista inicial. (1, 2, 3).
+
+    addNode(&head, 1);
+    addNode(&head, 2);
+    addNode(&head, 3);
+
+    printf("Lista inicial:\n");
+    printList(head);
+
+// Se llama a la funcion addToFront y se establece un nuevo nodo al inicio de la lista.
+
+    addToFront(&head, 0);
+    printf("Lista después de agregar un nodo al inicio:\n");
+    printList(head);
+
+// En esta parte se llama a la funcion addNNode y se establece un nuevo nodo al final de la lista. 
+
+    addNode(&head, 4);
+    printf("Lista después de agregar un nodo al final:\n");
+    printList(head);
+
+// En esta seccion
+
+    searchNode(head, 3);
+    searchNode(head, 5);
+
+// En esta seccion se declaran los elementos enesimos mediante la funcion getElementAt.
+
+    getElementAt(head, 2);
+    getElementAt(head, 5);
+
+// En esta parte se llama a la funcion deleteNode para eliminar nodos. 
+
+    deleteNode(&head, 2);
+    printf("Lista después de eliminar un nodo por valor:\n");
+    printList(head);
+
+    freeList(head); // Liberar la memoria al final del programa
+
+    return 0;
 }
